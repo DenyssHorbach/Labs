c42468a59a98992f8be8cbf79962a7aa397ddc79
DenyssHorbach <gorbachdenis200@gmail.com>
2025-09-17 08:59:25 +0300

    Create Task3.cpp

---
+++ Task3.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main() {
+  
+    int iVar1 = -34;
+    int iVar2 = iVar1;
+
+    int iVar3 = 98;
+    int iVar4 = -iVar3;
+
+    int iVar5 = 345, iVar6 = -55;
+    int iSum = iVar5 + iVar6;
+
+    int iVar7 = 43, iVar8 = 87;
+    int iSub = iVar7 - iVar8;
+
+    int iVar9 = 7, iVar10 = 23;
+    int iMul = iVar9 * iVar10;
+
+    int iVar11 = 18, iVar12 = 6;
+    int iDiv = iVar11 / iVar12;
+
+    int iVar13 = 18, iVar14 = 5;
+    int iDiv2 = iVar13 / iVar14;
+
+    int iVar15 = 19, iVar16 = 4;
+    int iRest = iVar15 % iVar16;
+
+    float fltVar17 = 81, fltVar18 = 10;
+    float fltDiv = fltVar17 / fltVar18;
+}
