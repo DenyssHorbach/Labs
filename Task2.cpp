5eac37f23b4a41214de5b78df2584052147dcbd3
DenyssHorbach <gorbachdenis200@gmail.com>
2025-09-17 08:58:42 +0300

    Create Task2.cpp

---
+++ Task2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+    const float CONST1 = 765.23e29;
+    float fltVar1;
+    fltVar1 = -123456789086.9;
+    float fltVar2 = 76.5e-400;
+
+
+    double dblVar3 = -0.435e-432;
+    double dblVar4;
+    dblVar4 = 9876.456e201;
+
+    return 0;
+    
+}
