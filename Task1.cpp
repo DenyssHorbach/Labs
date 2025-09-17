6bc7be03c6f12b0d60e37a4a54b434a1094b0c63
DenyssHorbach <gorbachdenis200@gmail.com>
2025-09-17 08:58:15 +0300

    Create Task1.cpp

---
+++ Task1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>;
+
+using namespace std;
+
+void main() {
+
+	const short CONST1 = 510;
+	short siVar1;
+	siVar1 = -88;
+	short siVar2 = 0x1c;
+
+	const int CONST2 = 337752;
+	int iVar3;
+	iVar3 = -61.37;
+	int iVar4 = 1024;
+
+	const long CONST3 = 2768;
+	long lVar5;
+	lVar5 = -97.8;
+	long lVar6 = 4015642735;
+
+	const unsigned short CONST4 = -65;
+	unsigned short wVar7;
+	wVar7 = 74234;
+	unsigned short wVar8 = 438;
+}
